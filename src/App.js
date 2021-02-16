578564a647f6600642565d67f189fc41bbaf793b
Sal Hamidi <hamidisa87@gmail.com>
2021-02-16 15:44:47 -0500

    Test commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+  return <h2>reminder project setup</h2>
+}
+test
+export default App
